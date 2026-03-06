c7152c2a09cbf6d81669b1021543ff051a39c857
dshishkin <79815076666@ya.ru>
2026-03-06 22:38:35 +0300

    Created classes user, ad, comment.

---
+++ src/main/java/ru/skypro/homework/entity/CommentEntity.java
@@ -0,0 +1,37 @@
+package ru.skypro.homework.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pk")
+    private Integer pk;
+
+    @Column(name = "text", nullable = false, length = 64)
+    private String text;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private UserEntity author;
+
+    @ManyToOne
+    @JoinColumn(name = "ad_id", nullable = false)
+    private AdEntity ad;
+}
